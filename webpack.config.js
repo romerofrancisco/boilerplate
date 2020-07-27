efeffcec742d84a620778f8b1a4cdc168f53d335
Francisco Romero <fjromerop@gmail.com>
2020-07-27 10:50:11 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path"); //This is a Node library being imported
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public/scripts"), //this resolves the absolute path
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/env"],
+          plugins: ["transform-object-rest-spread"]
+        },
+      },
+    }, ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+    publicPath: "/scripts/",
+    open: true,
+  },
+  devtool: "source-map",
+};
+
+// /home/tsaphkiel/Documents/Programming/Javascript Bootcamp/boilerplate
